982bbcf2ef055f216ddb1e7d4ea33c06d6ff8250
Hamidevity <127018991+Hamidevity@users.noreply.github.com>
2023-06-20 08:13:48 -0700

    Update Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <>
       <header className="header">
-        <h2>Botostart CRM</h2>
+        <h2> CRM</h2>
         <Link href="/add-customer">Add Customer</Link>
       </header>
       <div className="main">{children}</div>
